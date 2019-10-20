28890208d8de65e01b03581868ea441f94fa9164
Wecros <wecros@gmail.com>
2019-10-20 19:20:13 +0200

    First Enemy
    - made an enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	[SerializeField] List<Block> path;
+
+	// Use this for initialization
+	void Start () {
+		PrintAllWaypoints();
+	}
+
+	private void PrintAllWaypoints()
+	{
+		foreach (Block block in path)
+		{
+			print(block.name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
