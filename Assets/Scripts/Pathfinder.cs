53fea884d29e4c0f1eebb904525e2900eb5b0c17
Wecros <wecros@gmail.com>
2019-11-03 15:15:25 +0100

    minor changes 3/11/2019

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -7,12 +7,33 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left,        
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours() 
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoords = startWaypoint.GetGridPos() + direction;
+            print("Exploring " + explorationCoords);
+
+            try {
+                grid[explorationCoords].SetTopColor(Color.blue);                
+            }
+            catch { }
+        }
     }
 
     private void ColorStartAndEnd() 
