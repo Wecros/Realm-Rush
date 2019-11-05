245c40fb20c3ec2228655cc55587d152b3ca3a4e
Wecros <wecros@gmail.com>
2019-11-05 21:09:17 +0100

    Started Pathfinding algorithm

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,9 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    bool isRunning = true;
+
     Vector2Int[] directions = {
         Vector2Int.up,
         Vector2Int.right,
@@ -19,7 +23,28 @@ public class Pathfinder : MonoBehaviour
     {
         LoadBlocks();
         ColorStartAndEnd();
-        ExploreNeighbours();
+        Pathfind();
+        // ExploreNeighbours();
+    }
+
+    private void Pathfind() 
+    {
+        queue.Enqueue(startWaypoint);
+        while (queue.Count > 0) {
+            var searchCenter = queue.Dequeue();
+            print("Searching from: " + searchCenter);
+            HaltIfEndFound(searchCenter);
+        }
+        
+        print("Finished pathfinding?");
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (startWaypoint == endWaypoint) {
+            print("Searching from end node, therefore stopping");
+            isRunning = false;
+        }
     }
 
     private void ExploreNeighbours() 
