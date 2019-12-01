318312116d7544c3694a75cf09cf32791015d951
Wecros <wecros@gmail.com>
2019-12-01 20:38:24 +0100

    Spawning enemies repeatedly

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start a coroutine
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    private IEnumerator RepeatedlySpawnEnemies() {
+        while (true) {  // forever
+            print("Spawning");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
